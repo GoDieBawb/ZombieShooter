9c0752576131124b57927f89538c2ef03176256d
NegroBob <meancraft@hotmail.com>
2014-03-01 16:51:49 -0800

    Folder Rename
    Renamed the Folder to reflect the game

---
+++ ZombieShooter/mobile/gen/R.java.d
@@ -0,0 +1,8 @@
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\gen\com\mycompany\mygame\R.java \
+ : C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\layout\main.xml \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\res\values\strings.xml \
+C:\Users\Bob\Documents\GitHub\FromDarkness\FromDarkness\mobile\bin\AndroidManifest.xml \
